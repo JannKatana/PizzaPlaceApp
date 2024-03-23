2fec7fb30245241bd78c23f1064e549b1dddf31a
Mel-john Catanaoan <mjtcatanaoan@gmail.com>
2024-03-23 22:03:54 +0800

    Modified responses

--- src/API/PizzaPlaceApp.API/Controllers/ImportController.cs
+++ src/API/PizzaPlaceApp.API/Controllers/ImportController.cs
@@ -21,18 +21,18 @@ namespace PizzaPlaceApp.API.Controllers
         {
             if (file == null || file.Length == 0)
             {
-                return BadRequest("No file uploaded.");
+                return BadRequest(new { Message = "No file uploaded." });
             }
 
             // Check if the file is a CSV file
             if (Path.GetExtension(file.FileName).ToLower() != ".csv")
             {
-                return BadRequest("Invalid file format. Only CSV files are allowed.");
+                return BadRequest(new { Message = "Invalid file format. Only CSV files are allowed." });
             }
 
-            var response = await _mediator.Send(new ImportOrdersCommand { Data = file.OpenReadStream() });
+            await _mediator.Send(new ImportOrdersCommand { Data = file.OpenReadStream() });
 
-            return Ok("CSV file uploaded and processed successfully.");
+            return Ok(new { Message = "CSV file uploaded and processed successfully." });
         }
 
         [HttpPost("OrderDetails")]
@@ -40,18 +40,18 @@ namespace PizzaPlaceApp.API.Controllers
         {
             if (file == null || file.Length == 0)
             {
-                return BadRequest("No file uploaded.");
+                return BadRequest(new { Message = "No file uploaded." });
             }
 
             // Check if the file is a CSV file
             if (Path.GetExtension(file.FileName).ToLower() != ".csv")
             {
-                return BadRequest("Invalid file format. Only CSV files are allowed.");
+                return BadRequest(new { Message = "Invalid file format. Only CSV files are allowed." });
             }
 
-            var response = await _mediator.Send(new ImportOrderDetailsCommand { Data = file.OpenReadStream() });
+            await _mediator.Send(new ImportOrderDetailsCommand { Data = file.OpenReadStream() });
 
-            return Ok("CSV file uploaded and processed successfully.");
+            return Ok(new { Message = "CSV file uploaded and processed successfully." });
         }
     }
 }
